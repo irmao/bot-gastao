c5eb3d77d88c8814351f8edd99907a5ee2523820
Vinicius Dias <v.fernandesdias@gmail.com>
2017-10-19 10:33:04 -0200

    Returning sample http response in the format expected by the bot api

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 """ main module """
 
+from src.HandleRequest import handle_request
+
 def wsgi_app(environ, start_response):
     """ Parses the request and sends a reponse """
 
@@ -11,8 +13,9 @@ def wsgi_app(environ, start_response):
         request_body_size = int(environ.get('CONTENT_LENGTH', 0))
     except ValueError:
         request_body_size = 0
-    response_body = environ['wsgi.input'].read(request_body_size)
-    yield response_body
+    request_body = environ['wsgi.input'].read(request_body_size)
+    response_body = handle_request(request_body.decode('utf-8'))
+    yield response_body.encode('utf-8')
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
