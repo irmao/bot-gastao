49096d926f7b4a194f418f9300e33ffd5b0d5ad9
Vinicius Dias <v.fernandesdias@gmail.com>
2017-10-19 18:01:42 -0200

    Adding functions for checking balance and add expense

--- src/IntentFunctions.py
+++ src/IntentFunctions.py
@@ -1,20 +1,41 @@
 #!/usr/bin/python
 
 import json
+import src.StupidDatabase as StupidDatabase
+
+def extract_intent(request_json):
+    return request_json['result']['metadata']['intentName']
+
+def extract_value(request_json):
+    return int(request_json['result']['parameters']['value'][0])
 
 def add_expense_fn(request_json):
-    return 'add expense'
+    value = extract_value(request_json)
+    StupidDatabase.get_database().add_value(value)
+    message = ''
+    if value == 0:
+        message = 'Me poupe! Isso não faz diferença nenhuma'
+    elif value < 0:
+        message = 'Ok! Subtraí ' + str(-value) + 'reais do seu saldo'
+    else:
+        message = 'Ok! Adicionei ' + str(value) + ' reais no seu saldo'
+    return message
 
 def check_balance_fn(request_json):
-    return 'check balance'
+    value = StupidDatabase.get_database().get_value()
+    message = ''
+    if value == 0:
+        message = 'Sua conta está ZERADA. Pelo menos você não deve nada :)'
+    elif value < 0:
+        message = 'Você deve ' + str(-value) + ' reais'
+    else:
+        message = 'Você possui ' + str(value) + ' reais na sua conta'
+    return message
 
 functions = {
     'AddExpense'   : add_expense_fn,
     'CheckBalance' : check_balance_fn
 }
-
-def extract_intent(request_json):
-    return request_json['result']['metadata']['intentName']
 
 def get_speech(intent, request_json):
     function_to_call = functions[intent]
@@ -32,4 +53,4 @@ def test_extract_intent():
     fsample.close()
     request_json = json.loads(plain_request_json)
     intent = extract_intent(request_json)
-    assert intent == 'greetings'
+    assert intent == 'CheckBalance'
